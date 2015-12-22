cbfe138d33cef9d62ae84e668f955bc354dec6e2
Cyph3r <pankaj.sharma@whizdm.com>
2015-12-22 17:43:35 +0530

    Initial commit

---
+++ app/src/main/java/sharma/pankaj/nanodegree/MainActivity.java
@@ -0,0 +1,73 @@
+package sharma.pankaj.nanodegree;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.toolbar_tiitle_main_activity);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_main_spotify:
+                showToast("This button will launch spotify App");
+                break;
+            case R.id.btn_main_big:
+                showToast("This button will launch build it bigger App");
+                break;
+            case R.id.btn_main_library:
+                showToast("This button will launch library App");
+                break;
+            case R.id.btn_main_own:
+                showToast("This button will launch capstone App");
+                break;
+            case R.id.btn_main_reader:
+                showToast("This button will launch XYZ reader App");
+                break;
+            case R.id.btn_main_score:
+                showToast("This button will launch score App");
+                break;
+            default:
+
+                break;
+        }
+    }
+
+    private void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+}
